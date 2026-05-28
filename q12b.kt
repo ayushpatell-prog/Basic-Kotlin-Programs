dc545a8e4b8fe0269c2b56f777e0660dbbb88d57
Ayush Patel <ayushpatell796@gmail.com>
2026-05-28 15:38:09 +0530

    Add number swapping logic in main function w/o using 3rd var
    Implement a program to swap two numbers without a temporary variable.

---
+++ q12b.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var a = 20
+    var b = 60
+    println("Before swapping")
+    println("a = $a")
+    println("b = $b")
+    a=a+b
+    b=a-b
+    a=a-b
+    println("after swapping")
+    println("a = $a")
+    println("b = $b")
+}
