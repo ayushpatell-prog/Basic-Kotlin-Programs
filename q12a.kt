97249078400f3c9f002400e6cff6bb9c8b427f12
Ayush Patel <ayushpatell796@gmail.com>
2026-05-28 15:34:44 +0530

    Add Kotlin program to swap two numbers taking 3rd var

---
+++ q12a.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var a = 20
+    var b = 60
+    println("Before swapping")
+    println("a = $a")
+    println("b = $b")
+    var c = a
+    a = b
+    b = c
+    println("after swapping")
+    println("a = $a")
+    println("b = $b")
+}
