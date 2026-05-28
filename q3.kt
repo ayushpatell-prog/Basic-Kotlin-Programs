049f4541fd8dbee67940e2420df1973e76cdba16
Ayush Patel <ayushpatell796@gmail.com>
2026-05-28 15:53:01 +0530

    Refactor main function to use findSum

--- q3.kt
+++ q3.kt
@@ -1,8 +1,11 @@
-fun main() {
-    val N=10
+fun findSum(N: Int) {
     var sum = 0
-    for (i in 1..N step 1){
+    for (i in 1..N) {
         sum=sum+i
     }
     println("SUM = $sum")
-    }
+}
+fun main() {
+    val N = 10
+    findSum(N)
+}
