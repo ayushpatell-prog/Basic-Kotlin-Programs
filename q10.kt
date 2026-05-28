4ddc80a3f10120a13c4099be6eac072f4cb30e4d
Ayush Patel <ayushpatell796@gmail.com>
2026-05-28 15:26:11 +0530

    Add vowel and consonant check in main function

---
+++ q10.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val ch = 'i'
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+        println("$ch is a vowel")
+    }
+    else{
+       println("$ch is a consonant")
+    }
+}
