d25eef9c92af7634b45430be157a1af484f74b1c
Ayush Patel <ayushpatell796@gmail.com>
2026-05-28 15:16:37 +0530

    Add Kotlin program to find the largest of three numbers

---
+++ q7.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val a = 25
+    val b = 36
+    val c = 5
+    if (a > b && a > c){
+        println("$a is largest")
+    }
+    else if (b > a && b > c){
+        println("$b is largest")
+    }  
+    else{
+        println("$c is largest")
+    }
+}
