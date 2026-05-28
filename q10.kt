3ada459241c32621a6d6765247d579e2e9e7574a
Ayush Patel <ayushpatell796@gmail.com>
2026-05-28 15:30:37 +0530

    vowel or consonants check

--- q10.kt
+++ q10.kt
@@ -1,9 +1,10 @@
+fun checkVowel(s: Char) {
+    if (s=='a'||s=='e'||s=='i'||s=='o'||s=='u')
+        println("$s is a vowel")
+    else
+        println("$s is a consonant")
+}
 fun main() {
-    val ch = 'i'
-    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-        println("$ch is a vowel")
-    }
-    else{
-       println("$ch is a consonant")
-    }
+    val s = 'e'
+    checkVowel(s)
 }
