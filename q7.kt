dbff876f10e016886992402fa855a898b8b5c92a
Ayush Patel <ayushpatell796@gmail.com>
2026-05-28 15:57:12 +0530

    function to use largest function

--- q7.kt
+++ q7.kt
@@ -1,14 +1,17 @@
-fun main() {
-    val a = 25
-    val b = 36
-    val c = 5
-    if (a > b && a > c){
-        println("$a is largest")
-    }
-    else if (b > a && b > c){
-        println("$b is largest")
-    }  
-    else{
-        println("$c is largest")
-    }
+fun largest(a:Int,b:Int,c:Int){
+	if(a>b&&a>c){
+		println("$a is largest")
+	}
+	else if(b>a&&b>c){
+		println("$b is largest")
+	}
+	else{
+		println("$c is largest")
+	}
+}
+fun main(){
+	val a=25
+	val b=36
+	val c=5
+	largest(a,b,c)
 }
